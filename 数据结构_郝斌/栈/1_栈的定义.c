bcb3d4b78618f47615e47f7dad5f17f179eff385
rk717 <lianyilin98@gmail.com>
2020-06-06 22:27:21 +0200

    data_sturcture

---
+++ 数据结构_郝斌/栈/1_栈的定义.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+栈 stack
+内存可以分配静态内存和动态内存
+静态内存为栈 stack
+动态内存为堆 heap
+栈是吃完就吐， 队列是出完就拉
+
+栈的定义：
+    一种可以实现“先进后出“的存储结构
+    栈类似于箱子
+
+栈的分类：
+    静态栈
+    动态栈
+
+栈的算法：
+    出栈
+    压栈
+*/
+
+void f(int k)
+{
+    int m;
+
+    double* q = (double*)malloc(200);
+}
+
+int main(void)
+{
+    int i = 10;
+    int* p = (int*)malloc(200);
+    
+    return 0; 
+}
